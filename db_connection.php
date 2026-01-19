e06e4ef5529334343004f8d20c69413b42adabe9
Lego19A <anas2005h.AH@gmail.com>
2026-01-20 04:24:10 +0800

    Update website

--- db_connection.php
+++ db_connection.php
@@ -1,6 +1,6 @@
 <?php
 // Database connection parameters
-$servername = "localhost";
+$servername = "ljombusmysql.cdkkthw5xyg7.us-east-1.rds.amazonaws.com";
 $username = "jombus_user";  // default XAMPP username
 $password = "JomBusPass123!";      // default XAMPP password
 $dbname = "jom_bus";
